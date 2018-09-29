214ac45fdfaddb59f0017cabb8cd986cd60ca328
Jesse Xu <jessexu.mail@qq.com>
2018-09-29 21:56:12 +0800

    Initial Commit

---
+++ NIMKitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NIMKitDemo
+//
+//  Created by Jesse Xu on 2018/9/29.
+//  Copyright © 2018 fleetlabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
